3217727d0de5acddb2cdad594b751c3e526d44d7
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-02-07 12:49:28 -0500

    AJuste variables de entorno con los valores de la api de producción

--- core/config/enviromentVariables.ts
+++ core/config/enviromentVariables.ts
@@ -5,19 +5,21 @@ export const config = {
       ? process.env.EXPO_PUBLIC_PRODUCTION_EDU_PROMPT_API || ""
       : process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_API || "",
   eduPromptSocketUrl:
-    process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_SOCKET || "",
+    process.env.NODE_ENV === "production"
+      ? process.env.EXPO_PUBLIC_PRODUCTION_EDU_PROMPT_SOCKET || ""
+      : process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_SOCKET || "",
   tokenDailyRewardAmount:
     process.env.EXPO_PUBLIC_TOKEN_DAILY_REWARD_AMOUNT || "0",
   textResourcePrice: parseInt(
-    process.env.EXPO_PUBLIC_TEXT_RESOURCE_PRICE || "1"
+    process.env.EXPO_PUBLIC_TEXT_RESOURCE_PRICE || "1",
   ),
   imageResourcePrice: parseInt(
-    process.env.EXPO_PUBLIC_IMAGE_RESOURCE_PRICE || "2"
+    process.env.EXPO_PUBLIC_IMAGE_RESOURCE_PRICE || "2",
   ),
   tableResourcePrice: parseInt(
-    process.env.EXPO_PUBLIC_TABLE_RESOURCE_PRICE || "2"
+    process.env.EXPO_PUBLIC_TABLE_RESOURCE_PRICE || "2",
   ),
   chartResourcePrice: parseInt(
-    process.env.EXPO_PUBLIC_CHART_RESOURCE_PRICE || "2"
+    process.env.EXPO_PUBLIC_CHART_RESOURCE_PRICE || "2",
   ),
 };
