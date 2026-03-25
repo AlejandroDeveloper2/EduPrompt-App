b78d9463bd9aad96b6fab94e41bee031d4a5aee9
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-03-25 16:42:35 -0500

    fix: agregar mensaje de suscripción cancelada en traducciones y en el hook de cancelación

--- features/marketplace/hooks/mutations/useCancelSubscriptionMutation.ts
+++ features/marketplace/hooks/mutations/useCancelSubscriptionMutation.ts
@@ -2,18 +2,22 @@ import { useMutation } from "@tanstack/react-query";
 
 import { showToast } from "@/shared/context";
 
+import { useTranslations } from "@/shared/hooks/core";
+
 import { generateToastKey } from "@/shared/helpers";
 import { patchSubscriptionCancellation } from "../../services";
 
 const useCancelSubscriptionMutation = () => {
+  const { t } = useTranslations();
   return useMutation({
     mutationFn: patchSubscriptionCancellation,
     onSuccess: () => {
       showToast({
         key: generateToastKey(),
         variant: "primary",
-        message:
-          "Tu suscripción ha sido cancelada, puedes reactivarla cuando quieras.",
+        message: t(
+          "marketplace_translations.module_success_messages.plan_cancelled_msg",
+        ),
         toastDuration: 4000,
       });
     },
