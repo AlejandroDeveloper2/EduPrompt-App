9d3cf10e31f40d761f3e855aa1a80430b61147b8
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-03-25 12:01:37 -0500

    fix: actualizar configuración de variables de entorno para producción

--- core/config/enviromentVariables.ts
+++ core/config/enviromentVariables.ts
@@ -1,15 +1,23 @@
 /** Configuración de variables de entorno */
+const ENV = process.env.APP_ENV || process.env.NODE_ENV || "development";
+const isProduction = ENV === "production";
+
 export const config = {
-  eduPromptApiUrl:
-    process.env.NODE_ENV === "production"
-      ? process.env.EXPO_PUBLIC_PRODUCTION_EDU_PROMPT_API || ""
-      : process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_API || "",
-  eduPromptSocketUrl:
-    process.env.NODE_ENV === "production"
-      ? process.env.EXPO_PUBLIC_PRODUCTION_EDU_PROMPT_SOCKET || ""
-      : process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_SOCKET || "",
+  eduPromptApiUrl: isProduction
+    ? process.env.EXPO_PUBLIC_PRODUCTION_EDU_PROMPT_API || ""
+    : process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_API || "",
+
+  eduPromptSocketUrl: isProduction
+    ? process.env.EXPO_PUBLIC_PRODUCTION_EDU_PROMPT_SOCKET || ""
+    : process.env.EXPO_PUBLIC_DEVELOPMENT_EDU_PROMPT_SOCKET || "",
+
+  nextjsUrl: isProduction
+    ? process.env.EXPO_PUBLIC_PRODUCTION_NEXTJS_URL || ""
+    : process.env.EXPO_PUBLIC_NEXTJS_URL || "",
+
   tokenDailyRewardAmount:
     process.env.EXPO_PUBLIC_TOKEN_DAILY_REWARD_AMOUNT || "0",
+
   textResourcePrice: parseInt(
     process.env.EXPO_PUBLIC_TEXT_RESOURCE_PRICE || "1",
   ),
@@ -22,9 +30,6 @@ export const config = {
   chartResourcePrice: parseInt(
     process.env.EXPO_PUBLIC_CHART_RESOURCE_PRICE || "2",
   ),
+
   paypalClientId: process.env.EXPO_PUBLIC_PAYPAL_CLIENT_ID || "",
-  nextjsUrl:
-    process.env.NODE_ENV === "production"
-      ? process.env.EXPO_PUBLIC_PRODUCTION_NEXTJS_URL || ""
-      : process.env.EXPO_PUBLIC_NEXTJS_URL || "",
-};
+} as const;
